eac40c43b0917ae326a254f1fd87f35f983fd916
KevTee <32285956+KevTee@users.noreply.github.com>
2018-03-07 19:49:26 -0600

    Servlet Templates

---
+++ src/main/java/Servlets/WorkoutServlet.java
@@ -0,0 +1,14 @@
+package Servlets;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+public class WorkoutServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+	}
+
+	public void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+	}
+}
