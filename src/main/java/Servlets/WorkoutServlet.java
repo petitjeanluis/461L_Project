78b0f8b9cf4f41f1f09b19a3e6243c16a0f0b77f
KevTee <32285956+KevTee@users.noreply.github.com>
2018-03-07 19:53:30 -0600

    Added user service lines

--- src/main/java/Servlets/WorkoutServlet.java
+++ src/main/java/Servlets/WorkoutServlet.java
@@ -3,12 +3,19 @@ package Servlets;
 import java.io.IOException;
 import javax.servlet.http.*;
 
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
 public class WorkoutServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
+		
 	}
 
 	public void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
+		UserService userService = UserServiceFactory.getUserService();
+        User user = userService.getCurrentUser();
 	}
 }
