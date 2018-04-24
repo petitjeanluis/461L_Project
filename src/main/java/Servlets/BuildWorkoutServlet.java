7ff0eeda93f418967512474a4d6a2febc4c9dc71
Trevor Jones <trevorryanjones@gmail.com>
2018-04-23 22:19:54 -0500

    Changed to post from get for the workoutBuilderServlet

--- src/main/java/Servlets/BuildWorkoutServlet.java
+++ src/main/java/Servlets/BuildWorkoutServlet.java
@@ -13,28 +13,7 @@ import Database.*;
 public class BuildWorkoutServlet extends HttpServlet{
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
-		UserService userService = UserServiceFactory.getUserService();
-        User user = userService.getCurrentUser();
-        Storage storage = Storage.getInstance();
-        Client client = storage.loadClient(user);
-        
-        int numOfExercises = Integer.parseInt(req.getParameter("numOfExercises"));
-        String workoutName = req.getParameter("workoutName");
-        
-        ArrayList<Exercise> exercises = new ArrayList<Exercise>();
-        String base = "Exercise";
-        for(int i = 0; i < numOfExercises; i++) {
-        	Exercise e = storage.getExercise(req.getParameter(base + Integer.toString(i)));
-        	exercises.add(e);
-        }
-        
-        Workout customWorkout = new Workout(workoutName, exercises);
-        client.addCustomWorkout(customWorkout);
-        client.setCurrentWorkout(customWorkout);
-        
-        storage.saveClient(client);
-        
-        resp.sendRedirect("/workout.jsp");
+
 	}
 
 	public void doPost(HttpServletRequest req, HttpServletResponse resp)
